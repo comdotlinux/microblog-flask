47b9a9bf8f3af4b030043bf553a1910f74f0e05f
Guruprasad Kulkarni <guruprasad.kulkarni@idnow.de>
2018-02-16 23:59:54 +0100

    adding Posts

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from app import db
 
 
@@ -10,3 +12,10 @@ class User(db.Model):
 
 def __repr__(self):
     return '<User: id={} name={}>'.format(self.id, self.username)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))

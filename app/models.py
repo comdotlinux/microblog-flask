b7bd82fc987149eb1cc5d35c78c2edb9b38776eb
comdotlinux <guru@linux.com>
2018-02-17 18:05:38 +0100

    posts table

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 
 from app import db
 
@@ -13,11 +13,11 @@ class User(db.Model):
         return '<User: id={} name={}>'.format(self.id, self.username)
 
 
-# class Post(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     body = db.Column(db.String(140))
-#     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-#
-#     def __repr__(self):
-#         return '<Post: id={} timestamp={}>'.format(self.id, self.timestamp)
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post: id={} timestamp={}>'.format(self.id, self.timestamp)
